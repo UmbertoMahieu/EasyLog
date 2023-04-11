6b2fe4e796f5021b33517dcfd52c6af2f968d07e
UmbertoMahieu <115352867+UmbertoMahieu@users.noreply.github.com>
2023-04-11 18:34:02 +0200

    Update app.js
    creating function for each action

--- app.js
+++ app.js
@@ -1,47 +1,160 @@
-document.querySelector(".fa-phone").click();
+/// Functions
 
-var activity_type = document.querySelector(".o_Activity_summary").textContent;
+function call(){
+    document.querySelector(".fa-phone").click();
+}
+    
+function hang_up(){
+    document.querySelector('[aria-label="End Call"]').click(); 
+}
+    
+function get_acti(){
+    if(document.querySelector(".o_Activity_summary") != null){
+                return document.querySelector(".o_Activity_summary").textContent;
+    }
+}
+    
+function init_open_mailbox(){
 
-var open_mail = document.querySelector(".o_Composer_buttonFullComposer");
+    var open_mail = document.querySelector(".o_Composer_buttonFullComposer");
+        
+    if (open_mail===null){
 
-if (open_mail===null){
-    document.querySelector(".o_ChatterTopbar_buttonSendMessage").click();
+        return document.querySelector(".o_ChatterTopbar_buttonSendMessage").click();
+    }
 }
 
-setTimeout(function(){
-    document.querySelector(".o_Composer_buttonFullComposer").click();
-}, 2000);
+function open_mailbox(){
+    return document.querySelector(".o_Composer_buttonFullComposer").click();
+}
 
-setTimeout(function(){
+function open_mark_as_done(){
+    return document.querySelector(".o_Activity_markDoneButton").click();
+}
+
+function write_acti(acti){
+    return document.querySelector(".o_ActivityMarkDonePopoverContent_feedback").value = acti;
+}
+
+function mark_as_done(){
+    return document.querySelector(".o_ActivityMarkDonePopoverContent_doneButton").click();
+}
+
+function open_next_activity(){
+    document.querySelector(".o_ChatterTopbar_buttonScheduleActivity").click();
+}
+
+function next_opp(){
+    document.querySelector(".o_pager_next").click();
+}
+
+function send_mail(){
+
     var template_field = document.getElementById("template_id");
     var textarea = document.getElementById("body");
+
     template_field.value = "test";
+
     template_field.addEventListener('click', function() {
+
         var template_mail = document.querySelector("#template_id+ul>li>a");
+
         if(template_mail==null){
             setTimeout(function(){
                 template_field.dispatchEvent(new Event("click"));
             },1000);
             return
         }
-        template_mail.click();
-        setTimeout(function(){
+        else {
+            template_mail.click();
             document.getElementsByName("action_send_mail")[0].click();
+        }
+
+    });
+
+    template_field.click();
+}
+
+function planify_next_activity(){
+
+    var activity_field = document.getElementById("activity_type_id");
+    
+    activity_field.value = "Call";
+
+    activity_field.addEventListener('click', function() {
+
+        var template_acti = document.querySelector("#activity_type_id+ul>li>a");
+
+        if(template_acti==null){
+
+            setTimeout(function(){
+                
+                activity_field.dispatchEvent(new Event("click"));
+
+            },1000);
+
+            return
+
+        }
+
+        template_acti.click();
+
+        setTimeout(function(){
+            document.getElementsByName("action_close_dialog")[0].click();
         }, 2000);
     });
-    template_field.click();
-}, 3000);
+
+    activity_field.click();
+}
+
+/// Script
+
+//Calling 
+
+call();
 
 setTimeout(function(){
-    document.querySelector('[aria-label="End Call"]').click();
+    hang_up();
 }, 10000);
 
+// Send Email Template
+    
+current_acti = get_acti();
+        
+init_open_mailbox();
+
 setTimeout(function(){
-    document.querySelector(".o_Activity_markDoneButton").click();
+    open_mailbox();
+}, 2000);
+        
+setTimeout(function(){ 
+    send_mail();
+}, 3000);
+
+// Manage Current Activity
+            
+setTimeout(function(){
+    open_mark_as_done();
+        
     setTimeout(function(){
-        document.querySelector(".o_ActivityMarkDonePopoverContent_feedback").value = "na";
+        write_acti("na");
     }, 1000);
+            
     setTimeout(function(){
-        document.querySelector(".o_ActivityMarkDonePopoverContent_doneButton").click();
+        mark_as_done();    
     }, 1000);
 }, 7000);
+        
+// Planify Next Activity
+
+setTimeout(function(){
+    open_next_activity()
+}, 8000);
+
+setTimeout(function(){
+    planify_next_activity();
+}, 9000);
+        
+setTimeout(function(){
+    next_opp();
+}, 12000);
