1d51eab4045109d9a42ab00a78644be0c2fde398
UmbertoMahieu <115352867+UmbertoMahieu@users.noreply.github.com>
2023-04-21 19:22:19 +0200

    Update macro.js
    add functionnalities to send email based on activity and set next activity based on the actuel one

--- macro.js
+++ macro.js
@@ -16,7 +16,11 @@ function open_mark_as_done(){
     return document.querySelector(".o_Activity_markDoneButton").click();
 }
 
-function write_acti(acti){
+function write_acti_record(acti){
+    return document.querySelector(".o_ActivityMarkDonePopoverContent_feedback").value = acti;
+}
+
+function write_acti_type(acti){
     return document.querySelector(".o_ActivityMarkDonePopoverContent_feedback").value = acti;
 }
 
@@ -36,17 +40,50 @@ function next_opp(){
     document.querySelector(".o_pager_next").click();
 }
 
+function get_email_to_send(array_acti, array_mail, current_acti){
+    for (var i = 0 ; i < array_acti.length ; i++){
+        if (array_acti[i] == current_acti){
+            return array_mail[i];
+        }
+    }
+}
+
+function set_next_activity(array_acti, current_acti){
+    for (var i = 0 ; i < array_acti.length ; i++){
+        if (array_acti[i] == current_acti){
+            try{
+                next_acti = array_acti[i+1];
+                return true;
+            } catch (error){
+                console.error(error);
+                return false;
+            }
+        }
+    }
+}
+
 const macro = odoo.__DEBUG__.services["@web/core/macro"]; 
 const engine = new macro.MacroEngine(); 
-const acti = "na";
+
+// Settings variables
+const record_acti = "na";
+const department_acti = "Call"
+const array_acti = ["call 1", "call 2", "call 3"];
+const array_mail = ["email 1", "email 2", "email 3"];
+
+// Activity variables
+let current_acti = document.querySelector(".o_Activity_summary").textContent.replace(/”|“/g, '');
+let next_acti = set_next_activity(array_acti, current_acti);
 
 engine.activate({ 
     name: "callthemall", 
     steps: [
         {
+            // Launch the call
             action: () => call()
         }, 
         {
+            // Make sure we can open the mailbox
             action: () => init_open_mailbox()
         }, 
         {
@@ -54,46 +91,61 @@ engine.activate({
             action: "click"
         },
         {
+            // Insert the email template that need to be send
             trigger: '#template_id',
-            action: "text", value: "Welcome Demo"
+            action: "text", value: get_email_to_send(array_acti, array_mail, current_acti)
         },
         {
             trigger: "#template_id+ul>li>a",
             action: "click"
         },
         {
+            // Send the mail
             trigger: '[name="action_send_mail"]',
             action: "click"
         },
         {
+            // Open activity report window 
             action: () => open_mark_as_done()
         },
         {
+            // Add the basic "No Answer" report following an unreached call
             trigger: ".o_ActivityMarkDonePopoverContent_feedback",
-            action: () => write_acti(acti)
+            action: () => write_acti_record(record_acti)
         },
         {
+            // Validate the report
             action: () => mark_as_done()
         },
         {
+            // Planify Next Activity. Simply open the window
             action: () => open_next_activity()
         },
         {
+            // Insert Activity Type
             trigger: '#activity_type_id',
-            action: "text", value: "Call"
+            action: "text", value: department_acti
         },
         {
             trigger: "#activity_type_id+ul>li>a",
             action: "click"
         },
         {
+            // Insert Activity Summary
+            trigger: "#summary",
+            action: "text", value: next_acti
+        },
+        {
+            // Validate next Activity
             trigger: '[name="action_close_dialog"]',
             action: "click"
         },
         {
+            // Hang-up the call
             action: () =>  hang_up()
         },
         {
+            // Go to next opp
             action: () =>  next_opp()
         },
     ]
