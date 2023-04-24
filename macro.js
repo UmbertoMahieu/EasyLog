b04c31a6d0469862b8e12804d4f98031b3c7fbe8
UmbertoMahieu <115352867+UmbertoMahieu@users.noreply.github.com>
2023-04-24 12:16:43 +0200

    add getDate function, seTimout on hang-up
    add getDate function, seTimout on hang-up
    have small bug when closing next activity windows, it doesnt save the activity

--- macro.js
+++ macro.js
@@ -9,7 +9,7 @@ function init_open_mailbox(){
 }
 
 function call(){
-    document.querySelector(".fa-phone").click();
+    return document.querySelector(".fa-phone").click();
 }
 
 function open_mark_as_done(){
@@ -29,15 +29,15 @@ function mark_as_done(){
 }
 
 function open_next_activity(){
-    document.querySelector(".o_ChatterTopbar_buttonScheduleActivity").click();
+    return document.querySelector(".o_ChatterTopbar_buttonScheduleActivity").click();
 }
 
 function hang_up(){
-    document.querySelector('[aria-label="End Call"]').click(); 
+    return document.querySelector('[aria-label="End Call"]').click(); 
 }
 
 function next_opp(){
-    document.querySelector(".o_pager_next").click();
+    return document.querySelector(".o_pager_next").click();
 }
 
 function get_email_to_send(array_acti, array_mail, current_acti){
@@ -52,8 +52,7 @@ function set_next_activity(array_acti, current_acti){
     for (var i = 0 ; i < array_acti.length ; i++){
         if (array_acti[i] == current_acti){
             try{
-                next_acti = array_acti[i+1];
-                return true;
+                return next_acti = array_acti[i+1];
             } catch (error){
                 console.error(error);
                 return false;
@@ -61,6 +60,35 @@ function set_next_activity(array_acti, current_acti){
         }
     }
 }
+
+function getDate() {
+    const today = new Date();
+    let daysToAdd = 2;
+  
+    if (today.getDay() === 5) {
+      daysToAdd = 4;
+    }
+
+    if (today.getDay() === 4) {
+        daysToAdd = 4;
+        }
+
+    const futureDate = new Date(today.getTime() + (daysToAdd * 24 * 60 * 60 * 1000));
+  
+    if (futureDate.getDay() === 6) {
+      futureDate.setDate(futureDate.getDate() + 2);
+    }
+    if (futureDate.getDay() === 0) {
+      futureDate.setDate(futureDate.getDate() + 1);
+    }
+
+    const day = futureDate.getDate().toString().padStart(2, '0');
+    const month = (futureDate.getMonth() + 1).toString().padStart(2, '0');
+    const year = futureDate.getFullYear().toString();
+  
+    return `${day}/${month}/${year}`;
+  }
+  
 
 const macro = odoo.__DEBUG__.services["@web/core/macro"]; 
 const engine = new macro.MacroEngine(); 
@@ -74,6 +102,7 @@ const array_mail = ["email 1", "email 2", "email 3"];
 // Activity variables
 let current_acti = document.querySelector(".o_Activity_summary").textContent.replace(/”|“/g, '');
 let next_acti = set_next_activity(array_acti, current_acti);
+let next_acti_date = getDate();
 
 engine.activate({ 
     name: "callthemall", 
@@ -83,7 +112,14 @@ engine.activate({
             action: () => call()
         }, 
         {
+            trigger: '[aria-label="End Call"]',
+            action: () => setTimeout(() => {
+                  hang_up();
+                }, 15000)
+        },
+        {
             // Make sure we can open the mailbox
+            trigger: '[aria-label="Call"]',
             action: () => init_open_mailbox()
         }, 
         {
@@ -136,13 +172,14 @@ engine.activate({
             action: "text", value: next_acti
         },
         {
+            // Insert Activity Summary
+            trigger: '[data-target = "#o_datepicker_7"]',
+            action: "text", value: next_acti_date
+        },
+        {
             // Validate next Activity
             trigger: '[name="action_close_dialog"]',
             action: "click"
-        },
-        {
-            // Hang-up the call
-            action: () =>  hang_up()
         },
         {
             // Go to next opp
