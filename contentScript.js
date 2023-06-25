2fa79f008079a5ccc511743efd953ebb6f78f80c
UmbertoMahieu <115352867+UmbertoMahieu@users.noreply.github.com>
2023-06-25 13:15:58 +0200

    update

--- contentScript.js
+++ contentScript.js
@@ -14,8 +14,38 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 });
 
-  
 
 
+if (document.readyState !== 'loading') {
+    setTimeout(function(){
+        console.log("test")
+        // Create the "Call" button element
+        const callButton = document.createElement("button");
+        callButton.id = "callButton";
+        callButton.classList.add("btn", "btn-secondary");
+        callButton.setAttribute("data-tooltip", "Call");
+        callButton.title = "";
+        callButton.innerHTML = "<span>Call</span>";
+        
+        // Function to be executed when the "Call" button is clicked
+        function callButtonClick() {
+            // Your code here
+            console.log("Call button clicked");
+        }
+        
+        // Attaching an event listener to the "Call" button
+        callButton.addEventListener("click", callButtonClick);
+        
+        // Find the status bar container
+        const statusBar = document.querySelector(".o_statusbar_buttons");
+        
+        // Insert the "Call" button at the end of the status bar
+        statusBar.appendChild(callButton);
+    }, 2000)
     
-    
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log('document was not ready, place code here');
+        // myInitCode();
+    });
+}
